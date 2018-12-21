e6fe83f51f5c1f7982e6ff7b7cfbe641ef4153c4
Arun Seehra <seehra.a@gmail.com>
2018-12-21 10:18:34 -0600

    Refactor out a Pet component

---
+++ src/components/Pet/Pet.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Pet(props) {
+  const { pet, onAdoptPet } = props;
+
+  return (
+    <section>
+      <header>
+        <h2>{pet.name}</h2>
+        <img src={pet.imageURL} alt={pet.imageDescription} />
+      </header>
+      <main>
+        <dl>
+          <dt>Sex</dt>
+          <dd>{pet.sex}</dd>
+
+          <dt>Age</dt>
+          <dd>{pet.age}</dd>
+
+          <dt>Breed</dt>
+          <dd>{pet.breed}</dd>
+
+          <dt>Story</dt>
+          <dd>{pet.story}</dd>
+        </dl>
+
+        <button type="button" onClick={onAdoptPet}>Adopt</button>
+      </main>
+    </section>
+  );
+}
