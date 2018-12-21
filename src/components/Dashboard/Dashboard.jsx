983fcae759ad7e6453c123c212bfe5df299f4c53
Sam Dalton <spd4nf@virginia.edu>
2018-12-21 14:54:06 -0600

    working on copy for users

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -33,6 +33,22 @@ export class Dashboard extends React.Component {
           <h1>Petful Pet Adoption Agency</h1>
         </header>
         <main className="Dashboard__container">
+          <div className="Dashboard__info">
+            <h3>About Us</h3>
+            <p>
+              Welcome to the Petful Pet Adoption Agency! We run a tight ship around here, but we love
+              every pet that walks through the door. So long as they're a dog or cat, we'll take them and you can adopt them.
+            </p>
+            <h3>Rules</h3>
+            <ol>
+              <li>Your only choice is between adopting a cat or a dog.</li>
+              <li>You may not choose which dog or which cat you wish to take home.</li>
+              <li>We believe in a fair adoption process, so all dogs and cats are adopted on a first-in, first-out basis.</li>
+              <li>New dogs and cats given to the agency will be up for adoption after all previous cats or dogs have been adopted.</li>
+              <li>No fish, no rabbits, no rodents, no reptiles, no birds, no livestock.</li>
+              <li>NO TARANTULAS!!! Not after the last incident...</li>
+            </ol>
+          </div>
           <Pet pet={catToAdopt} loading={catLoading} onAdoptPet={onAdoptCat} className="Dashboard__pet"/>
           <Pet pet={dogToAdopt} loading={dogLoading} onAdoptPet={onAdoptDog} className="Dashboard__pet"/>
         </main>
